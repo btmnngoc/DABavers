a073d8577397bc66aa920468e2a2a074e5f29450
Bùi Trịnh Minh Ngọc <btmnngoc@gmail.com>
2025-05-14 23:59:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from data.loader import load_real_data
 from views.analysis import handle_analysis_menu
-from views.components import render_sidebar_header, render_footer, local_css
+from views.components import render_sidebar_header, render_footer, local_css, render_sidebar_footer
 from data.loader import load_stock_transaction_data
-from views.prediction import render_prediction_tab  # ✅from views.optimization import handle_optimization_menu
-
-
-
 import streamlit as st
+
+st.header('DABAFIN - PHÂN TÍCH TÀI CHÍNH DOANH NGHIỆP')
 
 def main():
     local_css()
     data = load_real_data()
     render_sidebar_header()
+    render_sidebar_footer()
     # 🔁 Thêm dữ liệu phân tích kỹ thuật
     transaction_data = load_stock_transaction_data()
     data.update(transaction_data)  # Hợp nhất vào dict `data`
@@ -22,10 +21,9 @@ def main():
     if menu_option == "Phân tích":
         handle_analysis_menu(data)
     elif menu_option == "Dự đoán":
-        render_prediction_tab()
+        st.warning('Chuc nang dang duoc xay dung')
     elif menu_option == "Tối ưu đầu tư":
-        handle_optimization_menu(data)
-
+        st.write('hi')
     render_footer()
 
 if __name__ == "__main__":
