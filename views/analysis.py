460416e31a58650ca37b7f21fc41e0ce3780542d
Bùi Trịnh Minh Ngọc <btmnngoc@gmail.com>
2025-05-15 02:02:34 +0700

    Update analysis.py

--- views/analysis.py
+++ views/analysis.py
@@ -1,4 +1,4 @@
-from views.components import render_financial_health, render_stock_movement, render_sector_indicators
+from views.components import render_financial_health, render_stock_movement
 import streamlit as st
 
 def handle_analysis_menu(data):
@@ -9,7 +9,7 @@ def handle_analysis_menu(data):
     ])
 
     if analysis_type == "Tổng quan":
-        render_sector_indicators(data)
+        st.write('hi')
     elif analysis_type == "Sức khỏe tài chính doanh nghiệp":
         stock = st.sidebar.radio("CHỌN MÃ", ["FPT", "CMG"])
         render_financial_health(data, stock)
