865b8976c1b848935ec7447d827e38355546f575
Bùi Trịnh Minh Ngọc <btmnngoc@gmail.com>
2025-05-15 17:52:13 +0700

    Update components.py

--- views/components.py
+++ views/components.py
@@ -322,6 +322,7 @@ def render_sector_indicators(csv_path, sector_name="Ngành CNTT"):
         'Q1_2024', 'Q2_2024', 'Q3_2024', 'Q4_2024'
     ]
     df_long['Period'] = df_long['Period'].astype(CategoricalDtype(categories=period_order, ordered=True))
+    df_long = df_long.sort_values(['Period'])
 
     indicator_groups = get_indicator_groups()
 
