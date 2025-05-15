ee7e115254a85682052a453cc9adbe2dba6c5f20
Bùi Trịnh Minh Ngọc <btmnngoc@gmail.com>
2025-05-15 18:26:03 +0700

    Update components.py

--- views/components.py
+++ views/components.py
@@ -322,7 +322,10 @@ def render_sector_indicators(csv_path, sector_name="Ngành CNTT"):
         'Q1_2024', 'Q2_2024', 'Q3_2024', 'Q4_2024'
     ]
     df_long = df_long.sort_values(['Period'])
-
+    df_long['Period'] = df_long['Period'].astype(CategoricalDtype(categories=period_order, ordered=True))
+    indicator_groups = get_indicator_groups()
+    # Tạo tabs
+    tabs = st.tabs(list(indicator_groups.keys()))
 
 
     for tab, (group_name, indicators) in zip(tabs, indicator_groups.items()):
