86594d3401218b9520be13fb18df719296e35955
Bùi Trịnh Minh Ngọc <btmnngoc@gmail.com>
2025-05-15 17:50:07 +0700

    Update analysis.py

--- views/analysis.py
+++ views/analysis.py
@@ -9,7 +9,7 @@ def handle_analysis_menu(data):
     ])
 
     if analysis_type == "Tổng quan":
-        render_sector_indicators(csv_path, sector_name="Ngành CNTT")
+        render_sector_indicators('assets/data/financial_metrics_it_processed.csv', sector_name="Ngành CNTT")
     elif analysis_type == "Sức khỏe tài chính doanh nghiệp":
         stock = st.sidebar.radio("CHỌN MÃ", ["FPT", "CMG"])
         render_financial_health(data, stock)
